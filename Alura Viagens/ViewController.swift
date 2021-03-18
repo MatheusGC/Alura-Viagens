6001f7b6c86bd1ab4e61f9e555109f789512da63
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-03-18 07:13:06 -0300

    Primeira tela

--- Alura Viagens/ViewController.swift
+++ Alura Viagens/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+  
+    
+    @IBOutlet weak var TabelaViagens: UITableView!
+    
+    let listaViagens:Array<String> = ["Rio de Janeiro", "Ceara", "São Paulo"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.TabelaViagens.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listaViagens[indexPath.row]
+        return cell
+    }
+    
+    
 
 }
 
