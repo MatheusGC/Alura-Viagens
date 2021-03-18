4db58e1284f63acf62505dfb5454186a28ba3d13
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-03-18 14:43:46 -0300

    Finalização do layout do app

--- Alura Viagens/Home/Controller/ViewController.swift
+++ Alura Viagens/Home/Controller/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let viagemAtual = listaViagens[indexPath.row]
         cell.labelTitulo.text = viagemAtual.titulo
         cell.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
-        cell.labelPreco.text = viagemAtual.preco
+        cell.labelPreco.text = "R$ \(viagemAtual.preco)"
         cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
         
         cell.imagemViagem.layer.cornerRadius = 10
@@ -45,7 +45,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-         return 175
+        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 175 : 260
+        
     }
 
 }
