e3e31444aa5ecbf50ee1636e208dd01fe35e1246
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-03-21 16:49:54 -0300

    Enviando e recebendo objeto entre controllers

--- Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class DetalhesViagensViewController: UIViewController {
 
+    @IBOutlet weak var imagemPacoteViagem: UIImageView!
+    @IBOutlet weak var labelTituloPacoteViagem: UILabel!
+    @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
+    @IBOutlet weak var labelDataViagem: UILabel!
+    @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
+    
+    var pacoteSelecionado:PacoteViagem? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let pacote = pacoteSelecionado {
+            self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
+            self.labelTituloPacoteViagem.text = pacote.viagem.titulo
+            self.labelDescricaoPacoteViagem.text = pacote.descricao
+            self.labelDataViagem.text = pacote.dataViagem
+            self.labelPrecoPacoteViagem.text = pacote.viagem.preco
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
 
-
+    @IBAction func botaoVoltar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
