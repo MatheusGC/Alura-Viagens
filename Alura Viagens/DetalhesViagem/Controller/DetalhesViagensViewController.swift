43f77509212eeadc4897d6a0adbca13bb258a68c
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-03-22 10:39:04 -0300

    Implementando ScrollView

--- Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class DetalhesViagensViewController: UIViewController {
 
+    
     @IBOutlet weak var imagemPacoteViagem: UIImageView!
     @IBOutlet weak var labelTituloPacoteViagem: UILabel!
     @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
     @IBOutlet weak var labelDataViagem: UILabel!
     @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
+    @IBOutlet weak var scrollPrincipal: UIScrollView!
     
     var pacoteSelecionado:PacoteViagem? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(aumentarScroll(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
         if let pacote = pacoteSelecionado {
             self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
@@ -32,6 +36,10 @@ class DetalhesViagensViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @objc func aumentarScroll(notification:Notification){
+        self.scrollPrincipal.contentSize = CGSize(width: self.scrollPrincipal.frame.width, height: self.scrollPrincipal.frame.height + 320)
     }
     
 
